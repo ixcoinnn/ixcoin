c752760288c2d83eea3aaf2152cdf37d0f68bd73
ixcoinnn <raizokenzi2003@gmail.com>
2026-04-09 00:45:57 +0800

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Fastify from "fastify";
 const app = Fastify();
 
 app.get("/", async () => {
-  return { status: "OK 🚀 Server hidup" };
+  return { status: "OK 🚀" };
 });
 
 app.listen({ port: process.env.PORT || 3000, host: "0.0.0.0" });
